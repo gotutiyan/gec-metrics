ed066364d00118c3620f9dde91079dca773340d6
gotuiyan <gto.rikujyo@icloud.com>
2024-11-16 19:39:59 +0900

    fixed gjg_test

--- gec_metrics/meta_eval/gjg_test.py
+++ gec_metrics/meta_eval/gjg_test.py
@@ -26,8 +26,8 @@ def test_corr_system():
     
     corr_cls = MetaEvalGJG.Corr
     gold = MetaEvalGJG.GJGSystemCorrOutput(
-        ew=corr_cls(pearson=-0.2315878996372926, spearman=-0.1758241758241758),
-        ts=corr_cls(pearson=-0.29331267502050523, spearman=-0.24725274725274726),
+        ew=corr_cls(pearson=-0.175093788793105, spearman=-0.016483516483516484),
+        ts=corr_cls(pearson=-0.2232996476764572, spearman=-0.06043956043956044),
     )
     for k in gold.__dict__:
         assert math.isclose(gold.__dict__[k].pearson, out.__dict__[k].pearson, rel_tol=1e-6)
